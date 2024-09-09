cb15904a66614ac24a91f5036e2fda43c7e3abed
MagdaTautu <tautu_magda@yahoo.com>
2024-09-09 18:45:54 +0300

    final

--- api/index.js
+++ api/index.js
@@ -60,6 +60,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
-app.listen(443, () => {
-    console.log("Server is running on port 443!!!");
+app.listen(3000, () => {
+    console.log("Server is running on port 3000!!!");
 });
